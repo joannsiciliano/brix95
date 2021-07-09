ae88658ebbaa1116a0bff0f2b587c2a8d896a1d3
JoAnn R Siciliano <77905621+joannsiciliano@users.noreply.github.com>
2021-07-09 11:53:08 -0600

    routes added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import { createGlobalStyle, ThemeProvider } from "styled-components";
 import original from "react95/dist/themes/original";
+import Strains from "./Strains";
+import Home from "./Home";
 
 import {
   styleReset,
   List,
   AppBar,
   ListItem,
-  Desktop,
   Toolbar,
   Divider,
   Button,
@@ -42,45 +44,58 @@ function App() {
     <div className="App">
       <GlobalStyles />
       <ThemeProvider theme={original}>
-        <AppBar theme={original}>
-          <Toolbar style={{ justifyContent: "space-between" }}>
-            <div style={{ position: "relative", display: "inline-block" }}>
-              <Button
-                onClick={() => setOpen(!open)}
-                active={open}
-                style={{ fontWeight: "bold" }}
-              >
-                {/* <img
+        <Router>
+          <AppBar theme={original}>
+            <Toolbar style={{ justifyContent: "space-between" }}>
+              <div style={{ position: "relative", display: "inline-block" }}>
+                <Button
+                  onClick={() => setOpen(!open)}
+                  active={open}
+                  style={{ fontWeight: "bold" }}
+                >
+                  {/* <img
                   src="🌿"
                   alt="react95 logo"
                   style={{ height: "20px", marginRight: 4 }}
                 /> */}
-                Start
-              </Button>
-              {open && (
-                <List
-                  style={{
-                    position: "absolute",
-                    left: "0",
-                    top: "100%",
-                  }}
-                  onClick={() => setOpen(false)}
-                >
-                  <Divider />
-                  <ListItem>
-                    <span role="img" aria-label="🔙">
-                      🌿
-                    </span>
-                    Strains
-                  </ListItem>
-                </List>
-              )}
-            </div>
+                  Start
+                </Button>
+                {open && (
+                  <List
+                    style={{
+                      position: "absolute",
+                      left: "0",
+                      top: "100%",
+                    }}
+                    onClick={() => setOpen(false)}
+                  >
+                    <Switch>
+                      <Divider />
+                      <Route component={Home} path="/" exact>
+                        <ListItem>
+                          <span role="img" aria-label="🏡"></span>
+                          🏡 Home
+                        </ListItem>
+                      </Route>
 
-            <TextField placeholder="Search..." width={150} />
-          </Toolbar>
-        </AppBar>
+                      <Divider />
+                      <ListItem>
+                        <Route component={Strains} path="/strains">
+                          <span role="img" aria-label="🌿">
+                            🌿
+                          </span>
+                          Strains
+                        </Route>
+                      </ListItem>
+                    </Switch>
+                  </List>
+                )}
+              </div>
+            </Toolbar>
+          </AppBar>
+        </Router>
       </ThemeProvider>
+      <h1> BRIX 95</h1>
     </div>
   );
 }
---
+++ src/Strains.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Strains() {
+  return (
+    <div className="Strains">
+      <h1> HELLO</h1>
+    </div>
+  );
+}
